d5e65f6159fe38fe7cf3dda6a9faf351ff9cbde3
salasmig <salasmig@gmail.com>
2015-10-10 16:00:33 +0100

    db access information
    Writing the access information required for connecting to the database

--- includes/config.php
+++ includes/config.php
@@ -4,9 +4,9 @@
 //mysql connection information
 
 $hostname_contacts = "localhost";  
-$database_contacts = "database_name"; //The name of the database
-$username_contacts = "database_username"; //The username for the database
-$password_contacts = "database_password"; // The password for the database
+$database_contacts = "booking_system"; //The name of the database
+$username_contacts = "booking_system"; //The username for the database
+$password_contacts = "booking"; // The password for the database
 $contacts = mysql_connect($hostname_contacts, $username_contacts, $password_contacts) or trigger_error(mysql_error(),E_USER_ERROR); 
 mysql_select_db($database_contacts, $contacts);
 
